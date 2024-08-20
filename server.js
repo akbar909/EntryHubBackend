c2ad8c640b49e82df4fa84e8b4598257171d1d65
unknown <jamaliakbar909@gmail.com>
2024-08-20 19:57:31 +0500

    make changes

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ const DB_URL = process.env.MONGO_DB_URL;
 
 app.use(express.json());
 
-app.use(cors({
-    origin: ['https://entry-hub-frontend.vercel.app/'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-}))
+app.use(cors())
+    // origin: ['https://entry-hub-frontend.vercel.app/'],
+    // methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    // credentials: true
+// }))
 
 app.get('/', (req, res) => {
     res.send('Hello World');
