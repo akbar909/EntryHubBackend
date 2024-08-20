ebfe88941e0d399ab4d3cf89b5c6b9ea488f2ce4
unknown <jamaliakbar909@gmail.com>
2024-08-20 20:24:14 +0500

    make changes

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -8,8 +8,8 @@ const biologyRoute = require('./routes/biologyRoute');
 const physicsRoute = require('./routes/physicsRoute');
 const mathRoute = require('./routes/mathRoute');
 const chemistryRoute = require('./routes/chemistryRoute');
+dotenv.config();
 const app = express();
-
 const DB_URL = process.env.MONGO_DB_URL;
 
 app.use(cors())
@@ -30,11 +30,17 @@ app.use('/mathquestions', mathRoute);
 app.use('/chemistryquestions', chemistryRoute);
 app.use('/users', userRoute);
 
-mongoose.connect(DB_URL).then(() => {
-    console.log('Connected to database');
-}).catch((error) => {
-    console.log(error);
-});
+console.log("Server is starting...");
+
+mongoose.connect(DB_URL)
+   .then(() => {
+      console.log('Connected to database');
+   })
+   .catch((error) => {
+      console.log('Database connection error:', error);
+   });
+
+console.log("Server setup complete");
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
